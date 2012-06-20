bfc22d117427bf47fc0831e46826157104fae649
Matthew Scott <matt@11craft.com>
2012-06-19 22:44:14 -0500

    Boilerplate

---
+++ README.rst
@@ -0,0 +1,8 @@
+Requirements
+============
+
+::
+
+    >>> import PySide
+    >>> PySide.__version__ >= '1.1.1'
+    True
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='kilometer-browser',
+    version='0.1',
+    description='Workhorse of a browser',
+    author='ElevenCraft Inc.',
+    author_email='kilometer-browser@11craft.com',
+    url='https://github.com/11craft/kilometer-browser',
+    install_requires=[
+        #
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points="""
+    """,
+)
